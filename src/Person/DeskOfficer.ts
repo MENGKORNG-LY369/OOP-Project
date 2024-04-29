660a521b5c6207a264106a3b0e4df5327254a026
MENGKORNG-LY <mengkorng.ly@student.passerellesnumeriques.org>
2024-04-29 15:45:33 +0700

    merge code from modify structure

--- src/Person/DeskOfficer.ts
+++
@@ -1,7 +0,0 @@
-import { Employee } from "./Employee";
-
-class DeskOfficer extends Employee{
-    constructor(firstName: string, lastName: string,email: string,age: number,gender: string,salary: number){
-        super(firstName,lastName,email,age,gender,salary);
-    }
-}
