e3626dfb06b411c4d4bd7f6296b80efde858e2cc
SANOK.PAYANG <SANOK.PAYANG@PNC.LAN>
2024-04-29 20:36:10 +0700

    ADDRESS

---
+++ src/Address/Airport/Gate/Gate.ts
@@ -0,0 +1,17 @@
+export class Gate {
+    constructor
+        (
+            private gateNumber: string,
+            private status: GateStatus
+
+
+        ) {
+
+        this.gateNumber = gateNumber
+        this.status = status
+    }
+    method(type): type{
+        
+    }
+
+}
