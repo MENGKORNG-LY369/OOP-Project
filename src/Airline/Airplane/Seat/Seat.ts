163830733f28368395ba096dd952284eaaa26766
SANOK.PAYANG <SANOK.PAYANG@PNC.LAN>
2024-04-30 12:28:55 +0700

    SEAT

---
+++ src/Airline/Airplane/Seat/Seat.ts
@@ -0,0 +1,28 @@
+export class Seat {
+
+    constructor
+        (
+            public seatNumber: string,
+            public type: SeatType,
+            public airplane: Airplane,
+            public price: number,
+            public booking?: Booking
+
+        ) {
+        this.seatNumber = seatNumber;
+        this.type = type;
+        this.airplane = airplane
+        this.price = price
+        this.booking = booking
+
+    }
+    getSeatType(): SeatType {
+        return this.type
+    }
+    getPrice(): number {
+        return this.price
+    }
+
+
+
+}
