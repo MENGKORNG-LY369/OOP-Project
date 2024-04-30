c16f21c1c903392d9de3a3fdf1ebbf197bec332c
SANOK.PAYANG <SANOK.PAYANG@PNC.LAN>
2024-04-30 12:22:56 +0700

    AIRLINE

---
+++ src/Airline/Airline.ts
@@ -0,0 +1,16 @@
+export class Airline {
+    constructor
+        (
+            private codeShare: string,
+            private airplane: Airplane[] = []
+        ) {
+        this.codeShare = codeShare
+        this.airplane = airplane
+
+    }
+    addAirplane(): void {
+
+    }
+
+
+}
