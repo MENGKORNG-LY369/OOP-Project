4b07ba51b9e3e37b66f255f3be875d4bee2da827
SANOK.PAYANG <SANOK.PAYANG@PNC.LAN>
2024-04-30 14:58:42 +0700

    AIRLINE

--- src/Airline/Airplane/Airplane.ts
+++ src/Airline/Airplane/Airplane.ts
@@ -1,18 +1,19 @@
+
 export class Plane {
     constructor
-        (
-            private registrationNumber: string,
+    (
+            private registerNumber: string,
             private layout: string,
             private airline: Airline,
             private cargo: Cargo[] = []
 
 
         ) {
-        this.registerNumber = this.registrationNumber
-        this.layout = this.layout
-        this.airline = this.airline
+        this.registerNumber = registerNumber
+        this.layout = layout
         this.airline = airline
-        this.cargo = this.cargo
+        this.airline = airline
+        this.cargo = cargo
 
     }
     getAirline(): Airline {
